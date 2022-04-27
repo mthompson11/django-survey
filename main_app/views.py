c78156ad6299585ea4cf3650fc65d5b1f0cfc371
mthompson11 <mthompson@Michaels-MacBook-Pro.local>
2022-04-27 08:46:42 -0700

    custom signup form

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+import re
 from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
@@ -5,6 +6,7 @@ from .models import Survey, Question
 from django.views.generic.edit import CreateView, DeleteView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CustomUserCreationForm
 
 def home(request):
   return render(request,'home.html')
@@ -14,7 +16,8 @@ def signup(request):
   if request.method == 'POST':
     # This is how to create a 'user' form object
     # that includes the data from the browser
-    form = UserCreationForm(request.POST)
+    #form = UserCreationForm(request.POST)
+    form = CustomUserCreationForm(request.POST)
     if form.is_valid():
       # This will add the user to the database
       user = form.save()
@@ -24,7 +27,8 @@ def signup(request):
     else:
       error_message = 'Invalid sign up - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
+  #form = UserCreationForm()
+  form = CustomUserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
